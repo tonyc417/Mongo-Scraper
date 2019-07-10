81f5656740501799258560f629f7c15208ac53c9
Tony Cruz <tonycruz417@gmail.com>
2019-07-10 18:04:31 -0500

    web scraper is now working

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const cheerio = require('cheerio');
 const axios = require('axios');
-const mongoose = require('mongoose');
 
 
 const PORT = process.env.PORT || 3000;
 
+axios.get('https://www.marketwatch.com/latest-news?mod=top_nav').then( (response) => {
+    var $ = cheerio.load(response.data);
+
+    var results = [];
+
+    $("h3.article__headline").each(function(i, element) {
+        var title = $(element).text();
+        var link = $(element).children().attr("href");
+
+        results.push({title, link});
+    });
+    console.log(results);
+});
