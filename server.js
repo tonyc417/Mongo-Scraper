18c370a76bcae15cba3448d4c068c7a4e5b21d0a
Tony Cruz <tonycruz417@gmail.com>
2019-07-10 18:26:06 -0500

    working on front page

--- server.js
+++ server.js
@@ -10,11 +10,19 @@ axios.get('https://www.marketwatch.com/latest-news?mod=top_nav').then( (response
 
     var results = [];
 
-    $("h3.article__headline").each(function(i, element) {
-        var title = $(element).text();
-        var link = $(element).children().attr("href");
+    // $("h3.article__headline").each(function(i, element) {
+    //     var title = $(element).text();
+    //     var link = $(element).children().attr("href");
 
-        results.push({title, link});
+    //     results.push({title, link});
+    // });
+
+    $("div.article__content").each(function(i, element) {
+        var title = $().children().text();
+        var link = $("h3.article__headline").children().attr("href");
+        var summ = $("p").children().attr("article__summary");
+
+        results.push({title, link, summ});
     });
     console.log(results);
 });
